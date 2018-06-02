4d61aae41e9d4d732dc727a184f5a8de4b2b3c86
T-107 <T-107@PC198.mshome.net>
2018-06-01 21:43:45 -0500

    asd

--- src/main/java/unitec/elementosmvc/MiUI.java
+++ src/main/java/unitec/elementosmvc/MiUI.java
@@ -5,14 +5,20 @@ package unitec.elementosmvc;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.tags.form.ButtonTag;
 
 @SpringUI
 public class MiUI extends UI{
+    //ponemos como atributo el repo
+    @Autowired RepositorioMensaje repoMensa;
 
     @Override
     protected void init(VaadinRequest request) {
@@ -25,9 +31,32 @@ public class MiUI extends UI{
         Button boton = new Button("Guardar Mensaje");
         boton.addStyleName(ValoTheme.BUTTON_DANGER);
         
+        //manejamos el evento de tipo button
+        boton.addClickListener(algo->{
+             String cuerpo = texto.getValue();
+             //guardamos
+             repoMensa.save(new Mensaje (cuerpo));
+             //Le cominicamos al usuario con un mensaje
+             Notification.show("Mensaje Guardado",Notification.Type.ERROR_MESSAGE);
+        });
+        
+        //caso 2 :Buscar todos
+        
+        Grid<Mensaje> grid = new Grid<>();
+        grid.setItems(repoMensa.findAll());
+
+grid.addColumn(Mensaje::getId).setCaption("ID");
+grid.addColumn(Mensaje::getCuerpo).setCaption("Cuerpo");
+/*grid.addColumn("ID");
+grid.addColumn("Cuerpo");*/
+        
+        
+        
         layout.addComponent(etiqueta);
         layout.addComponent(texto);
         layout.addComponent(boton);
+        //Agregamos nuestra componente  a nuesto layout
+        layout.addComponent(grid);
         
         setContent(layout);
         
