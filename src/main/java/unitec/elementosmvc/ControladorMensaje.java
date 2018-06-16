a6b83cc7cfc76ca1d1148f06fc2c3c4be06f93c0
T-107 <T-107@PC199.mshome.net>
2018-06-15 21:42:57 -0500

    holo

--- src/main/java/unitec/elementosmvc/ControladorMensaje.java
+++ src/main/java/unitec/elementosmvc/ControladorMensaje.java
@@ -1,8 +1,12 @@
 
 package unitec.elementosmvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class ControladorMensaje {
     
-    @GetMapping("/hola")
-    public String algo(){
-        return "hola desde el controlador";
-        }
+    @Autowired RepositorioMensaje repoMensa;
+    //Caso a) Buscar todos
+    
+    @GetMapping("/mensaje")
+    public List<Mensaje> buscarTodos(){
+    return repoMensa.findAll();
+    
+    }
+    
+    //Caso b) buscar por ID
+    @GetMapping("/mensaje/{id}")
+    public Mensaje buscarPorId(@PathVariable String id){//String es el tipo de dato del ID
+        Mensaje mensa = new Mensaje();
+        
+    return repoMensa.findById(id).get(); 
+    }
 }
